b5c8755ffb2651522045048b1c3c48a104c29ff2
VincentGonnet <vincent.gonnet08@gmail.com>
2023-07-13 15:01:33 +0200

    feat: exclude roles

--- bot.js
+++ bot.js
@@ -17,8 +17,9 @@ client.once('ready', () => {
 
 // CHANGE THOSE VALUES
 const MEDIA_CHANNEL_ID = process.env.MEDIA_CHANNEL_ID;
-const GIF_LIMIT = 3;
-const GIF_TIME_LIMIT = 90; // In seconds
+const ROLES_TO_EXCLUDE = ["1071866154315944108", "1125119655007232071"]; // ID's of the roles that won't be affected by the bot's rules. Leave empty if you don't want to exclude any role.
+const GIF_LIMIT = 2;
+const GIF_TIME_LIMIT = 120; // In seconds
 const WARNING_TITLE = "Tu envoies trop de gifs !";
 const WARNING_MESSAGE = "Merci de ne pas spammer les gifs.\nTu peux les envoyer dans le salon <#" + MEDIA_CHANNEL_ID + "> à la place.";
 
@@ -28,6 +29,11 @@ client.on('messageCreate', message => {
     if (message.author.bot) return;
 
     if (message.channel.id != MEDIA_CHANNEL_ID && (message.content.startsWith("https://tenor.com/view/") || (message.content.includes("giphy.com") && message.content.includes("media")))) {
+
+        // If the user has a role that is excluded from the bot's rules, don't do anything.
+        for (let i = 0; i < ROLES_TO_EXCLUDE.length; i++) {
+            if (message.member.roles.cache.has(ROLES_TO_EXCLUDE[i])) return;
+        }
 
         // If the user has sent more than X gifs in the last Y seconds, delete the message and send them a warning.
         if (gifMap.has(message.author.id) && gifMap.get(message.author.id) >= GIF_LIMIT) {
